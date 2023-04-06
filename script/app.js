448723848e4f10d6866b16ce6381c2ed120bae89
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2023-04-06 15:26:59 +0400

    added

--- script/app.js
+++ script/app.js
@@ -93,3 +93,12 @@ function change_keys(e) {
     }, 800);
   }
 }
+
+
+
+document.body.addEventListener("pointermove", (e)=>{
+  const { currentTarget: el, clientX: x, clientY: y } = e;
+  const { top: t, left: l, width: w, height: h } = el.getBoundingClientRect();
+  el.style.setProperty('--posX',  x-l-w/2);
+  el.style.setProperty('--posY',  y-t-h/2);
+})
